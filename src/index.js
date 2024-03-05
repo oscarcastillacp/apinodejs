74405d88538ffbec69423c84d1f74631dd3f8f5e
oscarcastillacp <106272996+oscarcastillacp@users.noreply.github.com>
2024-03-04 23:04:14 -0600

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js'
-import {PORT} from './config.js'
-
-
-app.listen(PORT);
-
-console.log('server running on port',PORT);
--- src/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import {ping} from '../controllers/index.contoller.js'
-const router= Router();
-
-router.get('/pin',ping);
-
-
-
-export default router;
